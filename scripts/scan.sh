1ee3310e2b3717cbbd6c545e916574920e9c7f3b
Minhaz Mahmud <minhaz.m2@gmail.com>
2015-06-03 20:02:34 -0400

    turns out we do need to do force scan... temp fox for now

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,2 +1,2 @@
-# Start scan of library
-LD_LIBRARY_PATH=/usr/lib/plexmediaserver /usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh
+# Force scan and refresh of library
+LD_LIBRARY_PATH=/usr/lib/plexmediaserver /usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --force
